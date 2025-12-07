113d7d48f7d79e311eca46da4b074de9eb3db84c
hasinduudara <hasindutechschool@gmail.com>
2025-12-07 20:05:57 +0530

    Add navigation for Home and Details screens in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+          name="index" // matches the file name
+          options={{
+              title: 'Home',
+          }}
+      />
+      <Stack.Screen
+          name="details" // matches the file name
+          options={{
+              title: 'Details',
+          }}
+      />
+    </Stack>
+  );
 }
